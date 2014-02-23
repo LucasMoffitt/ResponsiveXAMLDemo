3eff33cfac9edc93ddfd2bc69a2a9b4e852fe90d
Lucas Moffitt <LucasMoffitt@Gmail.com>
2014-02-23 14:21:17 +1100

    initial code commit

---
+++ src/XamlStateManagementDemo/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace XamlStateManagementDemo
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            SizeChanged += MainPage_SizeChanged;
+        }
+
+        private void MainPage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            VisualStateManager.GoToState(this, GetState(e.NewSize.Width), true);
+            ScreenWidth.Text = e.NewSize.Width.ToString();
+        }
+        
+        private string GetState(double width)
+        {
+            if (width <= 500)
+                return "Snapped";
+
+            if (width <= 660)
+                return "EvenSmaller";
+
+            if (width <= 755)
+                return "Smaller";
+
+            return "Default";
+        }
+    }
+}
